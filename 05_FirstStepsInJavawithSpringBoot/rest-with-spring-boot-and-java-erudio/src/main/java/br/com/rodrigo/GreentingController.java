036f2a5e62e3ff2709baecbbb48bfc4ecd3255ac
Rodrigo Amaro Pereira <=>
2024-08-16 08:48:48 -0300

    First Steps in Java with Spring Boot 3

---
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/rodrigo/GreentingController.java
@@ -0,0 +1,21 @@
+package br.com.rodrigo;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreentingController {
+	
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping("/greenting")
+	public Greeting greeting(
+			@RequestParam(value = "name", defaultValue = "World")
+			String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+}
